b673ed6f847486679171264269f480b2ba28d6d1
Dani Postigo <dani@helloalfred.com>
2015-09-08 15:43:51 -0400

    changes

--- Pod/Classes/AYSource.swift
+++ Pod/Classes/AYSource.swift
@@ -4,15 +4,42 @@
 
 import Foundation
 
-public class AYSource: NSObject {
+@objc(AYSource) public class AYSource: NSObject {
 
     public var index: Int = 0
 
-    public var items: [AnyObject]
+    public var items: [AnyObject] = []
+
+    public var count: Int {
+        return self.items.count
+    }
 
 
-    init(index: Int, items: [AnyObject] = []) {
+    public subscript(index: Int) -> AnyObject {
+        get {
+            return self.items[index]
+        }
+        set {
+            self.items[index] = newValue
+        }
+    }
+
+
+
+    public init(index: Int, items: [AnyObject] = []) {
         self.index = index
         self.items = items
     }
+
+
+    public func move(item: AnyObject, destination: AYSource, index: Int) {
+        let array = self.items as NSArray
+        let index = array.indexOfObject(item)
+
+        self.items.removeAtIndex(index)
+        destination.items.insert(item, atIndex: index)
+
+
+    }
+
 }
